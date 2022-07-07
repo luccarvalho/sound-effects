833a4711f2e714af3c8e1673ce64ca047dddb5b9
Lucas Carvalho <102173672+luccarvalho@users.noreply.github.com>
2022-07-07 16:54:19 -0300

    Add files via upload
    Fazendo algumas alterações na estilização do projeto (CSS) e melhorando o meu código JavaScript.

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-    
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
     //template string
     const idAudio = `#som_${instrumento}`;
-    
-    //console.log(idAudio);
-    
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    //console.log(contador);
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
